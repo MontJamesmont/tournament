1eb7d6476f95212a99809632aaf8fbc816a34168
unknown <chyba ja@.(none)>
2014-11-08 13:37:29 +0100

    zmiany w modelu i bazie

--- main/models.py
+++ main/models.py
@@ -16,10 +16,16 @@ class Category(models.Model):
     name=models.CharField(max_length=50)
 
 class Coach(models.Model):
-    user_id = models.ForeignKey(User)
-
+    user_id = models.ForeignKey(User, null=True)
+    name = models.CharField(max_length=32)
+    surname = models.CharField(max_length=32)
+    team_id = models.ForeignKey(Team)
+    
 class Player(models.Model):
-    user_id = models.ForeignKey(User)
+    user_id = models.ForeignKey(User, null=True)
+    name = models.CharField(max_length=32)
+    surname = models.CharField(max_length=32)
+    team_id = models.ForeignKey(Team)
     
 class Manager(models.Model):
     user_id = models.ForeignKey(User)
@@ -29,7 +35,7 @@ class Tournament(models.Model):
     name = models.CharField(max_length=50)
     start = models.DateField()
     end = models.DateField()
-    manager = models.ForeignKey(Manager)
+    manager = models.ForeignKey(Manager, null=True)
     coaches = models.ManyToManyField(Coach, verbose_name="lista trenerow", blank=True)
     players = models.ManyToManyField(Player, verbose_name="lista graczy", blank=True)
     categories = models.ManyToManyField(Category, verbose_name="lista kategorii", blank=True)
